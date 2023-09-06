690d17087b11c093b50d95de31cf5b14513f630a
wpqlks7 <wpqlks7@gmail.com>
2023-09-06 10:44:04 +0900

    230906

---
+++ 230906/1_4.py
@@ -0,0 +1,17 @@
+print('Hello Python!')
+"""
+print("Hello Python!')
+따옴표의 매칭이 정상적으로 되지 않음.
+"""
+
+print(100 + 200)
+"""
+print(100 + '200')
+정수형에 문자열을 더했음.
+"""
+
+print('Hello Python!')
+"""
+print(Hello Python!)
+문자열에 따옴표를 찍어주지 않았음.
+"""
---
+++ 230906/1_5.py
@@ -0,0 +1,1 @@
+print('Welcome to Python!!\n' * 5)
