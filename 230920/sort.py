9c65b34c9c9cd89fad7d401d32bf5e9ef28776d2
wpqlks7 <wpqlks7@gmail.com>
2023-09-20 10:11:10 +0900

    230920_2

--- 230920/sort.py
+++ 230920/sort.py
@@ -2,8 +2,15 @@ a, b, c = input("세 정수를 입력하시오 : ").split()
 a = int(a)
 b = int(b)
 c = int(c)
-first = min(a,b,c)
-last = max(a,b,c)
+
+if(a>=b and a>=c): last = a
+elif(b>=c and b>=a): last = b
+else: last = c
+
+if(a<=b and a<=c): first = a
+elif(b<=c and b<=a): first = b
+else: first = c
+
 middle = (a+b+c)-first-last
 
 print("오름차순으로 나열:", first, middle, last)
