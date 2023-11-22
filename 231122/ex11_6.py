6c4106950017413a98e940e278b5837975a1e451
wpqlks7 <wpqlks7@gmail.com>
2023-11-22 11:04:41 +0900

    231122_2

--- 231122/ex11_6.py
+++ 231122/ex11_6.py
@@ -13,7 +13,10 @@ for i in range(9):
     if max < a[i + 1]:
         max = a[i + 1]
 
-print("a = {}".format(a))
-print("최댓값 : {}".format(max))
-print("최솟값 : {}".format(min))
-print("평균값 : {}".format(avg/10))
+print("a = [ {:.8f}".format(a[0]), end=" ")
+for i in range(9):
+    print("{:.8f} ".format(a[i+1]),end="")
+print("]")
+print("최댓값 : {:.16f}".format(max))
+print("최솟값 : {:.16f}".format(min))
+print("평균값 : {:.16f}".format(avg/10))
